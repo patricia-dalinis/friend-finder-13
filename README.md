da4b3f6a81b95ed93ed0dc5cae171a94abffd1d6
patricia-dalinis <47152085+patricia-dalinis@users.noreply.github.com>
2019-06-21 23:59:22 -0500

    Updated Pics and Added Heroku Link to ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # friend-finder-13
+
+Heroku Link: https://friend-finder-13.herokuapp.com/
+
+ReadMe Pending...
--- app/data/friends.js
+++ app/data/friends.js
@@ -6,23 +6,23 @@ var friends = [
 	},
 	{
 		name: "Ellen Ripley",
-		photo: "https://pmcvariety.files.wordpress.com/2017/05/ellen-ripley-alien.jpg?w=1000",
+		photo: "https://media3.giphy.com/media/SYmkILRsQTFLO/giphy.gif?cid=790b76115d0db50264386856323198f0&rid=giphy.gif",
 		scores: [4, 1, 1, 3, 5, 2, 1, 2, 4, 5]
 	},
 	{
 		name: "Evelyn Carnahan",
-		photo: "https://vignette.wikia.nocookie.net/p__/images/a/a7/We%27re_supposed_to_believe_that%27s_Rachel_Wies_in_the_third_one%3F.jpeg/revision/latest/scale-to-width-down/2000?cb=20190421032841&path-prefix=protagonist",
+		photo: "https://media2.giphy.com/media/VDAoznkmqHwPu/giphy.gif?cid=790b76115d0db52e3654527067920070&rid=giphy.gif",
 		scores: [5, 2, 2, 5, 1, 5, 1, 3, 1, 5]
 	},
 	{
 		name: "Domino",
-		photo: "https://pmcvariety.files.wordpress.com/2017/06/rexfeatures_9691437l-1.jpg?w=1024",
+		photo: "https://media1.giphy.com/media/9DegO8AfoNZmVLm6AF/giphy.gif?cid=790b76115d0db53f4b726d2f63b2bec6&rid=giphy.gif",
 		scores: [1, 5, 2, 2, 3, 2, 4, 5, 5, 5]
 	},
 	{
 		name: "Rey",
-		photo: "https://en.wikipedia.org/wiki/File:Rey_Star_Wars.png",
-		scores: [3, 2, 5, 5, 5, 1, 1, 3, 3, 4]
+		photo: "https://media1.giphy.com/media/mntlYrVH06RmDps1iG/giphy.gif?cid=790b76115d0db558376d2e533697f23b&rid=giphy.gif",
+		scores: [3, 2, 5, 5, 5, 1, 1, 3, 3, 5]
 	}
 ];
 
